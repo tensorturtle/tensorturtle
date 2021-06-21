952d9dc25be2976e3239ae659ddd3273403a7ebd
tensorturtle <tensorturtle@gmail.com>
2021-06-21 10:20:16 +0900

    update dotfiles

--- PopOS_setup/scripts/upload.sh
+++ PopOS_setup/scripts/upload.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
 DEST='../dotfiles'
 cp ~/.bash_aliases $DEST
 cp ~/.bashrc $DEST
 cp ~/.alacritty.yml $DEST
 cp ~/.tmux.conf $DEST
-cp ~/.config/nvim/init.vim $DEST
+#cp ~/.config/nvim/init.vim $DEST
 
