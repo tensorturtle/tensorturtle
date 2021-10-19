0e5fa8fe68aba9d812f6473f3c7254883df5999c
tensorturtle <tensorturtle@gmail.com>
2021-10-19 16:12:33 +0900

    add mount unraid script with correct permission

--- PopOS_setup/scripts/mount-unraid.sh
+++ PopOS_setup/scripts/mount-unraid.sh
@@ -1,12 +1,16 @@
 # USAGE:
 # ./mount-unraid name_of_share
-echo "Make sure 'cifs-utils' is installed (sudo apt install cifs-utils)"
+
+# 'uid=1000,gid=1000' modifies the permissions to be writable by regular user (otherwise, only sudo works.
+# to find uid and gid, use:
+# 'id -u <user>
+# 'id -g <user>
 
 IP_ADDR="172.30.1.147"
 sudo mkdir ~/unraid
 cd ~/unraid
 sudo mkdir $1_share
-sudo mount -t cifs -o user=jason //$IP_ADDR/$1 ~/unraid/$1_share
+sudo mount -t cifs -o user=jason,uid=1000,gid=1000 //$IP_ADDR/$1 ~/unraid/$1_share
 
 echo "SUCCESS! '$1' share has been mounted to ~/unraid/$1_share."
 echo "Use 'sudo umount ~/unraid/$1_share' to unmount drive"
