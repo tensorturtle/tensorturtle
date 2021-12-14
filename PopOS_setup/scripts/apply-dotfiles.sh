35c9789967e103dadf8da13ed83a7b80f932751a
tensorturtle <tensorturtle@gmail.com>
2021-12-14 19:52:42 +0900

    wezterm script and dotfile for linux

--- PopOS_setup/scripts/apply-dotfiles.sh
+++ PopOS_setup/scripts/apply-dotfiles.sh
@@ -1,3 +1,6 @@
+# idempotent create config dir
+mkdir -p ~/.config/wezterm
+
 # copies dotfiles here to home folder on system
 
 cd ..
@@ -8,6 +11,7 @@ cp .bash_aliases ~
 cp .bashrc ~
 cp init.vim ~/.config/nvim
 cp .tmux.conf ~
+cp wezterm.lua ~/.config/wezterm
 
 # source bashrc file to refresh
 source ~/.bashrc
--- PopOS_setup/scripts/upload.sh
+++ PopOS_setup/scripts/upload.sh
@@ -5,4 +5,5 @@ cp ~/.bashrc $DEST
 cp ~/.alacritty.yml $DEST
 cp ~/.tmux.conf $DEST
 #cp ~/.config/nvim/init.vim $DEST
+cp ~/.config/wezterm/wezterm.lua $DEST
 
